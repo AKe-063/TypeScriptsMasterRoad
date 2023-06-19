aff2950bc045f33f8184fda83d553e649e03c00e
zhengyuke <zhengyuke@Kingsoft.com>
2023-06-19 19:26:10 +0800

    feat: 提交装饰器相关测试

--- Task02/Decorator.ts
+++ Task02/Decorator.ts
@@ -1,13 +1,50 @@
-// 类装饰器
-function classDecorator(target: any) {
+// 普通类装饰器
+function ClassDecorator(target: any) {
+    target.anyKey = "xx";
     console.log(target);
 }
 
-// @classDecorator
-// class ClassDecoratorExample {
-//     public getName() {
-//         return this.name;
-//     }
+// 装饰器工厂
+function ClassDecoratorFactory(name: string) {
+    return function(target: any) {
+        console.log(`Hello ${name} has Class name ${target["name"]}!`);
+    }
+}
 
-//     private name: string;
-// }
+// 属性装饰器
+function PropertyDecorator(propertyName: string) {
+    return function(target: any, propertyKey: string) {
+        console.log(`propertyName: ${propertyName}, propertyKey: ${propertyKey}`);
+        console.log("constructor name: " + target.constructor.name);
+    }
+}
+
+// 方法装饰器
+function FuncDecorator(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("FuncDecorator: ");
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+}
+
+// 参数装饰器
+function ParamDecorator(target: any, propertyKey: string, parameterIndex: number) {
+    console.log("ParamDecorator: ");
+    console.log(target);
+    console.log(propertyKey);
+    console.log(parameterIndex);
+}
+
+// @ClassDecorator
+@ClassDecoratorFactory("decoratorFactoryTest")
+class ClassDecoratorExample {
+    constructor(testName: string) {
+        this.testName = testName;
+    }
+
+    @FuncDecorator
+    public testFunc(@ParamDecorator testParam: number) {}
+
+    @PropertyDecorator("PropertyDecoratorTest")
+    public testName: string = "decorator";
+}
