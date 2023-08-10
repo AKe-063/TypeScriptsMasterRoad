67af34f409079adf79a7316563082d99bf9916bb
顾里南 <915105422@qq.com>
2023-08-10 15:05:58 +0800

    feat: deepClone

--- Task12/DeepClone.ts
+++ Task12/DeepClone.ts
@@ -1,10 +1,22 @@
 // TypeScript 实现一个深拷贝对象的方法，不允许使用序列化的的方法，要求支持泛型
 function deepClone<T extends Object>(proto: T): any {
-    let copy = {};
-    Object.keys(proto).forEach((property) => {
-        copy[property] = proto[property];
-    });
-    return copy;
+    if (Array.isArray(proto)) return proto.map((element) => deepClone(element));
+
+    if (proto instanceof Map) {
+        let copyMap = new Map();
+        proto.forEach((value, key) => copyMap.set(deepClone(key), deepClone(value)));
+        return copyMap;
+    }
+
+    if (typeof proto === `object`) {
+        let copy: any = {};
+        Object.keys(proto).forEach((property) => {
+            copy[property] = deepClone((proto as unknown as any)[property]);
+        });
+        return copy;
+    }
+
+    return proto;
 }
 
 // 普通对象
@@ -21,7 +33,9 @@ console.log(obj2[1] === cloned2[1]); // false
 console.log((obj2[1] as any)[1] === cloned2[1][1]); // false
 
 // Map 对象
-const obj5 = new Map([[{ a: 1 }, { b: 2 }]]);
+const key = { a: 1 };
+const obj5 = new Map([[key, { b: 2 }]]);
 const cloned5 = deepClone(obj5);
 console.log(obj5 === cloned5); // false
-console.log(obj5.get({ a: 1 }) === cloned5.get({ a: 1 })); // false
+console.log(obj5.get(key) === cloned5.get(key)); // false
+console.log(`obj5's value: ${obj5.get(key)}, and cloned5's value: ${cloned5.get(key)}.`);
