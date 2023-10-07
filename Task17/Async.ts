11335d1f3e8d3d8d7e687f9965eba000797c7d35
zhengyuke <zhengyuke@Kingsoft.com>
2023-10-07 11:33:28 +0800

    feat: 增加 type/node

--- Task17/Async.ts
+++ Task17/Async.ts
@@ -1,2 +1,6 @@
 // 给定 5 个 txt 文件，内容自定义，要求异步读取其中的数据，并当所有内容都加载完后，
 // 异步写到另外一个文件中，要求了解 await 和 async，并包含异常处理，要求使用 promiseAll
+import fs from "fs";
+
+const readFilePaths = fs.readdirSync("./readFile");
+console.log(readFilePaths);
